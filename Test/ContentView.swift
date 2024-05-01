7ca03d9ad1571c27857038070ce70452860e3ae2
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-05-01 16:51:07 +0700

    done

--- Test/ContentView.swift
+++ Test/ContentView.swift
@@ -1,42 +1,40 @@
 //
 //  ContentView.swift
-//  Test
+//  WalletTest
 //
 //  Created by Thang Ho Duc on 01/05/2024.
 //
 
 import SwiftUI
 import SwiftData
-
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            Button(action:createWallet) {
+                Text("Create wallet")
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
+    }
+    
+    public func createWallet(){
+        let plg = AwesomeLibrary();
+        let mnemonic = "bacon grit piece velvet shallow mail hidden obey visa copy race certain"
+        
+        let path = "44'/60'/0'/0/0"
+        
+        let rs: String = plg.hdkey(mnemonicString: mnemonic, path: path).self
+      
+        let address = rs.split(separator: " ")[0]
+        
+        print("address : ", address)
+        
+        let publicKey = rs.split(separator: " ")[1]
+        print("publicKey : ", publicKey)
+        let privateKey = rs.split(separator: " ")[2]
+        print("privateKey: ", privateKey)
     }
 
     private func addItem() {
